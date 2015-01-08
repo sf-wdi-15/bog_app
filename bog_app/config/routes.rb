b03a208eb6c6e5099b5ab42e8a7ff7340530ea05
Samuel Cabral <samfcabral@gmail.com>
2015-01-07 22:34:46 -0800

    Homework complete

---
+++ bog_app/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  root to: "bogs#index"
+  
+  get '/bogs', to: 'bogs#index'
+
+  get '/bogs/new', to: 'bogs#new'
+
+  get '/bogs/:id', to: 'bogs#show'
+
+  get '/bogs/:id/edit', to: 'bogs#edit'
+
+  post '/bogs', to: 'bogs#create'
+
+  put '/bogs/:id', to: 'bogs#update'
+
+end
